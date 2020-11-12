60ee16faaddcc8a35651449fbd3104516d60a307
Natividad Alvarez <tivialvarez@gmail.com>
2020-11-12 13:52:25 -0500

    Added crud methods to users model

--- models/users.js
+++ models/users.js
@@ -1,6 +1,10 @@
 const db = require("../data/db");
 const bcrypt = require("bcryptjs");
 
+function getUsers(username=""){
+    return db("users").where("username", "like", `${username}%`).select("email", "username", "pfp", "location");
+}
+
 function getByUsername(username){
     return db("users").where({username}).first();
 }
@@ -18,7 +22,46 @@ async function createUser(email, username, password, pfp, location){
     return newUser
 }
 
+async function updateUser(userId, changes){
+    if(!userId) throw new Error("User id must be provided");
+    //all updates should be optional, so we'll take in an object of fields with their changes
+    //whatever properties are added on the changes object will be the only properties that change    
+    //ex. a req.body of {username: 'newUsername'} will update the username but leave all other fields the same
+    //ex. a req.body of {email: "email@gmail.com", pfp: 'someurl.com'} will update email and pfp only but leave all other fields the same
+    
+    //the following makes sure only fields that need to be updated are updated
+    //and only valid fields are passed to .update
+    changes = {
+        email: (changes.email)? changes.email: undefined, 
+        username: (changes.username)? changes.username: undefined,
+        password: (changes.password)? bcrypt.hashSync(changes.password, process.env.NUM_OF_HASHES): undefined,
+        pfp: (changes.pfp)? changes.pfp: undefined,
+        location: (changes.location)? changes.location: undefined
+    }
+    // knex won't allow us to push an empty object to the .update method
+    // this variable tracks if the changes object only has undefined values
+    // if it does, we will skip the update query and just return the user object as is
+    let changesExist = false;
+    for(let field in changes){
+        if (changes[field]) changesExist = true;
+    }
+
+    if(changesExist) await db("users").where({id: userId}).update(changes);
+    return db("users").where({id: userId}).first();
+}
+
+async function deleteUser(userId){
+    if(!userId) throw new Error("User id must be provided");
+    
+    const userToDelete = await db("users").where({id: userId}).first();
+    if(userToDelete) await db("users").where({id: userId}).del();
+    return userToDelete;
+}
+
 module.exports = {
+    getUsers,
     getByUsername,
-    createUser
+    createUser,
+    updateUser,
+    deleteUser
 }
