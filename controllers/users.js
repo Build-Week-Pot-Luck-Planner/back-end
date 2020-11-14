c9622b5f07757968cfc7a2944a1cc3fbfbac1812
Natividad Alvarez <tivialvarez@gmail.com>
2020-11-13 19:26:35 -0500

    added method to get single user's info on users controller

--- controllers/users.js
+++ controllers/users.js
@@ -11,6 +11,20 @@ async function getUsers(req, res){
     }
 }
 
+async function getUser(req, res){
+    const userId = req.params.userId;
+    if(req.user.id != userId) return res.status(403).json({message: "Unauthorized"});
+    
+    try {
+        const user = await db.getUserWithPotlucks(userId);
+        return res.status(200).json({user});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: "A server error occurred", err});
+    }
+}
+
 module.exports = {
-    getUsers
+    getUsers,
+    getUser
 }
