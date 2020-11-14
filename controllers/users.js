132fa8a8ec85f47bacc9a95602c8c16cd8f22d5a
Natividad Alvarez <tivialvarez@gmail.com>
2020-11-14 09:41:02 -0500

    Added users controller method to update user info

--- controllers/users.js
+++ controllers/users.js
@@ -24,7 +24,25 @@ async function getUser(req, res){
     }
 }
 
+async function updateUser(req, res){
+    const userId = req.params.userId;
+    if(req.user.id != userId) return res.status(403).json({message: "Unauthorized"});
+    
+    try {
+        const userExists = await db.getById(userId);
+        if(!userExists) return res.status(404).json({message: "User not found"});
+
+        const updatedUser = await db.updateUser(userId, req.body);
+        if(updatedUser.err) return res.status(400).json({message: updatedUser.err});
+        res.status(200).json({message: "Your account info was updated", user: updatedUser});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: "A server error occurred", err});
+    }
+}
+
 module.exports = {
     getUsers,
-    getUser
+    getUser,
+    updateUser
 }
