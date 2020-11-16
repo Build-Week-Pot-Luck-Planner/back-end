553f6e47a4a669bab05f11dcb6540bfce752bc39
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-11-15 18:10:54 -0800

    small changes made

--- routes/invitations.js
+++ routes/invitations.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const inviteController = require("../controllers/invitations");
 const { restricted } = require("../middleware/auth");
 router.get(
-	"/potlucks/:id/invitation",
+	"/potlucks/:id/invitations",
 	restricted,
 	inviteController.getInvitedGuests
 );
@@ -12,12 +12,16 @@ router.get(
 	restricted,
 	inviteController.getPotluckInvites
 );
-router.post("/potlucks/:id/invitation", restricted, inviteController.post);
+router.post("/potlucks/:id/invitations", restricted, inviteController.post);
 router.put(
-	"/:potluckId/invitation/:inviteId",
+	"/potlucks/:potluckId/invitations/:inviteId",
 	restricted,
 	inviteController.put
 );
-router.delete("/potlucks/:id/invitation", restricted, inviteController.del);
+router.delete(
+	"/potlucks/:id/invitations/:inviteId",
+	restricted,
+	inviteController.del
+);
 
 module.exports = router;
--- routes/items.js
+++ routes/items.js
@@ -7,7 +7,7 @@ router.get("/potlucks/:id/items", restricted, itemController.getItems);
 router.get("/users/:id/items", restricted, itemController.getItemsUserNeedsToBring);
 router.post("/potlucks/:id/items", restricted, itemController.post);
 router.put("/potlucks/:id/items/:itemId", restricted, itemController.put);
-router.delete("potlucks/:id/items/:itemId", restricted, itemController.del);
+router.delete("/potlucks/:id/items/:itemId", restricted, itemController.del);
 
 
 
