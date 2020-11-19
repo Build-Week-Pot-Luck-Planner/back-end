b2343f3e602fd3679a7b0b26f07c51524dc7a01e
Natividad Alvarez <tivialvarez@gmail.com>
2020-11-18 20:42:40 -0500

    Covnerting NUM_OF_HASHES to number in seed data

--- data/seeds/01Users.js
+++ data/seeds/01Users.js
@@ -10,17 +10,17 @@ exports.seed = function (knex) {
 				{
 					email: "test@gmail.com",
 					username: "Test",
-					password: bcrypt.hashSync("password", process.env.NUM_OF_HASHES),
+					password: bcrypt.hashSync("password", Number(process.env.NUM_OF_HASHES)),
 				},
 				{
 					email: "bob@gmail.com",
 					username: "Bob",
-					password: bcrypt.hashSync("password", process.env.NUM_OF_HASHES),
+					password: bcrypt.hashSync("password", Number(process.env.NUM_OF_HASHES)),
 				},
 				{
 					email: "sarah@gmail.com",
 					username: "sarah",
-					password: bcrypt.hashSync("password", process.env.NUM_OF_HASHES),
+					password: bcrypt.hashSync("password", Number(process.env.NUM_OF_HASHES)),
 				},
 			]);
 		});
